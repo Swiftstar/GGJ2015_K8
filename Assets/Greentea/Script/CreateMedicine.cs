d24d5a5f156ebbf1e3cd66bcf21ce51ffc756c0e
HaunSheng <greentea122622@gmail.com>
2015-01-24 15:31:04 +0800

    hi my name is Pan Huan Sheng

---
+++ Assets/Greentea/Script/CreateMedicine.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateMedicine : MonoBehaviour {
+
+    public GameObject MedicineObj;
+    public Transform[] MedicinePosition;
+
+    private float EveryMedicineCreateTime = 0.3f;
+    private float CreateTime = 1f;
+    private int Level = 1;
+
+    void Start()
+    {
+        StartCoroutine("CreateTimer");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Level = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Level = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Level = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Level = 4;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Level = 5;
+        }
+    }
+
+    IEnumerator CreateTimer()
+    {
+        yield return new WaitForSeconds(CreateTime);
+        ChoiceMode();
+    }
+
+    private int ModeCount = 0;
+
+    private int pastPartMode = 0;
+    private int thisPartMode = 0;
+
+    public List<int> thisModeRandom = new List<int>();
+    
+    void ChoiceMode()
+    {
+        thisPartPosition.Clear();
+        ModeCount = Level * 4;
+
+        thisPartMode = Random.Range(1, ModeCount + 1);
+        Debug.Log(thisPartMode);
+        pastPartMode = thisPartMode;
+        StartCoroutine("CreateTimer");
+    }
+
+    public List<Transform> thisPartPosition = new List<Transform>();
+
+
+}
---
+++ Assets/Greentea/Script/MedicineButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MedicineButton : MonoBehaviour
+{
+    public GameObject MedicineObj;
+
+    public float GetHPTime = 2;
+
+	void Start () 
+    {
+        UIEventListener.Get(MedicineObj).onClick = DestroyAnimation;
+        StartCoroutine("GetHPTimer");
+	}
+
+    IEnumerator GetHPTimer()
+    {
+        yield return new WaitForSeconds(GetHPTime);
+        Debug.Log("I got HP");
+        //GetHPAnimationPlay;
+    }
+
+    void DestroyAnimation(GameObject e)
+    {
+        Debug.Log("This Medicine is be destroyed");
+        //DestroyAnimationPlay
+    }
+}
