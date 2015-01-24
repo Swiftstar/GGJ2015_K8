6f2760d7e1be544f5600e682881c3468879c3d6f
HenrySwiftstar <h79121@gmail.com>
2015-01-24 15:11:29 +0800

    Add Liver Life

--- Assets/Scripts/CONST.cs
+++ Assets/Scripts/CONST.cs
@@ -6,5 +6,22 @@ public static class CONST {
 	public const string NAME_SPRITE_LIVER_BG = "Sprite_LiverBG";
 	public const string NAME_SPRITE_LIVER_DARK = "Sprite_LiverDark";
 	public const string NAME_BTN_START = "Btn_Start";
+	public const string NAME_LBL_START = "Lbl_Start";
+
+	public static readonly string[] NAME_LIVER_BLOCK = 
+	{
+		"Sprite_Liver01",
+		"Sprite_Liver02",
+		"Sprite_Liver03",
+		"Sprite_Liver04",
+		"Sprite_Liver05",
+		"Sprite_Liver06",
+		"Sprite_Liver07",
+		"Sprite_Liver08",
+		"Sprite_Liver09",
+		"Sprite_Liver10"
+	};
+
+	public const string WIN_RETRY = "Win!!Retry?";
 
 } // end +CONST
